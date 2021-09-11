3d312f5affd5ba1710b8d6841d60ae07a3fc2d41
Tafowo Cedric <tafowocedric@gmail.com>
2021-09-11 13:21:51 +0100

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   root: true,
